88daf5184d745836402a3ddf41514326839f5d4d
kiluazen <kushalsokke@gmail.com>
2025-06-23 22:29:45 +0530

    hmm

--- build.sh
+++ build.sh
@@ -2,10 +2,14 @@
 # Exit on error
 set -o errexit
 
+export CARGO_HOME="$HOME/.cargo"
+export RUSTUP_HOME="$HOME/.rustup"
+mkdir -p "$CARGO_HOME" "$RUSTUP_HOME"
+
 # Upgrade pip
 python -m pip install --upgrade pip
 
 # Install requirements without cache and with no binary for problematic packages
-pip install --no-cache-dir --upgrade -r requirements.txt
+pip install --prefer-binary --no-cache-dir --upgrade -r requirements.txt
 
 echo "Build completed successfully!" 
